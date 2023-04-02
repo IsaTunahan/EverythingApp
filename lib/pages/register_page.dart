54a3af4512ed5cb75dc0d9ee17e67029070b7559
IsaTunahan <118114545+IsaTunahan@users.noreply.github.com>
2023-04-03 00:13:01 +0300

    Google Sign In Service Kuruldu

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -133,19 +133,7 @@ class _RegisterPageState extends State<RegisterPage> {
           
                 const SizedBox(height: 10),
           
-                // forgot password?
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
+                
           
                 const SizedBox(height: 25),
           
